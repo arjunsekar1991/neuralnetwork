fd4f089df7742359e5575b1b70c9bba4b9a1d4d9
Arjun <arjunsekar1991@gmail.com>
2019-11-01 17:06:36 -0400

    layers definition complete

--- SimpleNeuralNetwork.py
+++ SimpleNeuralNetwork.py
@@ -1,3 +1,4 @@
+import random
 class NeuralNetwork:
     def __init__(self, inputNeurons, hiddenLayers, outputNeurons):
         self.inputNeurons = inputNeurons
@@ -9,8 +10,26 @@ class NeuralNetwork:
     def buildMultiLayerPercepton(self):
         print("hi")
 
+        if len(self.hiddenLayers) == 0:
+            self.multiLayerPercepton.append(self.layer(self.inputNeurons, self.outputNeurons))
+        else:
+            self.multiLayerPercepton.append(self.layer(self.inputNeurons, self.hiddenLayers[0]))
+            print(len(self.hiddenLayers))
+            for i in range(1, len(self.hiddenLayers)):
+                self.multiLayerPercepton.append(self.layer(self.hiddenLayers[i-1], self.hiddenLayers[i]))
+            self.multiLayerPercepton.append(self.layer(self.hiddenLayers[-1], self.outputNeurons))
 
-cls = NeuralNetwork(1,[1],1)
 
+    def layer(self,numberOfInputs,numberOfOutputs):
+        layer=[]
+        for outputCounter in range(numberOfOutputs):
+            thetas = [random.random() for i in range(numberOfInputs)]
+            neuron = {"thetas":thetas,"output":None,"delta":None}
+            layer.append(neuron)
+        return layer
+
+
+cls = NeuralNetwork(2,[3],2)
+print(cls.multiLayerPercepton)
 
 

07a6b4a78a8089e1ee3347e9a6da43106100a047
Arjun <arjunsekar1991@gmail.com>
2019-10-31 21:10:20 -0400

    commiting intial version

--- NeuralNetworkFromScratch.py
+++
@@ -1,8 +0,0 @@
-class NeuralNetwork:
-    def __init__(self, inputNeurons, hiddenLayers, outputNeurons):
-        self.inputNeurons = inputNeurons
-        self.hiddenLayers=hiddenLayers
-        self.outputNeurons=outputNeurons
-        self.multiLayerPercepton = multiLayerPercepton()
-
-
---
+++ SimpleNeuralNetwork.py
@@ -0,0 +1,16 @@
+class NeuralNetwork:
+    def __init__(self, inputNeurons, hiddenLayers, outputNeurons):
+        self.inputNeurons = inputNeurons
+        self.hiddenLayers=hiddenLayers
+        self.outputNeurons=outputNeurons
+        self.multiLayerPercepton = []
+        self.buildMultiLayerPercepton()
+
+    def buildMultiLayerPercepton(self):
+        print("hi")
+
+
+cls = NeuralNetwork(1,[1],1)
+
+
+

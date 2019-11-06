49eefad0b1af4e41b736cbf9a0d6b7c3b214bce4
Arjun <arjunsekar1991@gmail.com>
2019-11-06 00:54:13 -0500

    remove unwanted lines

--- SimpleNeuralNetwork.py
+++ SimpleNeuralNetwork.py
@@ -11,95 +11,94 @@ import warnings
 import matplotlib.pyplot as plt
 warnings.filterwarnings('ignore')
 
-def buildNetwork(inputNeurons, hiddenLayers, outputNeurons):
-    neuralNetwork = list()
-    inputLayer = [{'thetas':[random.random() for thetaCounter in range(inputNeurons + 1)]} for i in range(inputNeurons)]
-    neuralNetwork.append(inputLayer)
-    for index,hiddenNeurons in enumerate(hiddenLayers):
-        #print("important logic",index,hiddenNeurons)
-        if len(hiddenLayers) == 1:
-            #print("this must not run")
-            hiddenLayer = [{'thetas':[random.random() for thetaCounter in range(inputNeurons + 1)]} for i in range(hiddenNeurons)]
-            neuralNetwork.append(hiddenLayer)
-        else:
-            if index==0:
-                #print("index is zero")
+class NeuralNetwork:
+    def __init__(self,inputNeurons, hiddenLayers, outputNeurons):
+        self.neuralNetwork = self.buildNetwork(inputNeurons, hiddenLayers, outputNeurons)
+
+    def buildNetwork(self,inputNeurons, hiddenLayers, outputNeurons):
+        neuralNetwork = list()
+        inputLayer = [{'thetas':[random.random() for thetaCounter in range(inputNeurons + 1)]} for i in range(inputNeurons)]
+        neuralNetwork.append(inputLayer)
+        for index,hiddenNeurons in enumerate(hiddenLayers):
+            #print("important logic",index,hiddenNeurons)
+            if len(hiddenLayers) == 1:
+                #print("this must not run")
                 hiddenLayer = [{'thetas':[random.random() for thetaCounter in range(inputNeurons + 1)]} for i in range(hiddenNeurons)]
                 neuralNetwork.append(hiddenLayer)
             else:
-                hiddenLayer = [{'thetas':[random.random() for thetaCounter in range(hiddenLayers[index - 1] + 1)]} for i in range(hiddenNeurons)]
-                neuralNetwork.append(hiddenLayer)
-    outputLayer = [{'thetas':[random.random() for thetaCounter in range(hiddenLayers[-1] + 1)]} for i in range(outputNeurons)]
-    neuralNetwork.append(outputLayer)
+                if index==0:
+                    #print("index is zero")
+                    hiddenLayer = [{'thetas':[random.random() for thetaCounter in range(inputNeurons + 1)]} for i in range(hiddenNeurons)]
+                    neuralNetwork.append(hiddenLayer)
+                else:
+                    hiddenLayer = [{'thetas':[random.random() for thetaCounter in range(hiddenLayers[index - 1] + 1)]} for i in range(hiddenNeurons)]
+                    neuralNetwork.append(hiddenLayer)
+        outputLayer = [{'thetas':[random.random() for thetaCounter in range(hiddenLayers[-1] + 1)]} for i in range(outputNeurons)]
+        neuralNetwork.append(outputLayer)
 
-    return neuralNetwork
-
-
-
-
-
-def feedForwardPropagation(neuralNetwork, X):
-    for eachLayer in neuralNetwork:
-        outputs = []
-        for neurons in eachLayer:
-            activation = calculateZ(neurons['thetas'], X)
-            neurons['output'] = calculateSigmoid(activation)
-            outputs.append(neurons['output'])
-        X = outputs
-    return X
+        return neuralNetwork
 
 
 
-def backward_propagate_error(network, expected):
-    for i in reversed(range(len(network))):
-        layer = network[i]
-        #print(layer)
-        errors = list()
 
-        if i != len(network)-1:
-            for j in range(len(layer)):
-                error = 0.0
-                for neuron in network[i + 1]:
-                    error += (neuron['thetas'][j] * neuron['delta'])
-                errors.append(error)
-        else:
+
+    def feedForwardPropagation(self,neuralNetwork, X):
+        for eachLayer in neuralNetwork:
+            outputs = []
+            for neurons in eachLayer:
+                Z = self.calculateZ (neurons['thetas'], X)
+                neurons['output'] = self.calculateSigmoid(Z)
+                outputs.append(neurons['output'])
+            X = outputs
+        return X
+
+
+
+    def backPropagation(self, network, expected):
+        for layerCounter in reversed(range(len(network))):
+            layer = network[layerCounter]
+
+            cost = list()
+
+            if layerCounter != len(network)-1:
+                for j in range(len(layer)):
+                    error = 0.0
+                    for neuron in network[layerCounter + 1]:
+                        error += (neuron['thetas'][j] * neuron['delta'])
+                    cost.append(error)
+            else:
+                for j in range(len(layer)):
+                    neuron = layer[j]
+                    cost.append(expected[j] - neuron['output'])
+
             for j in range(len(layer)):
                 neuron = layer[j]
-                errors.append(expected[j] - neuron['output'])
-
-        for j in range(len(layer)):
-            neuron = layer[j]
-            neuron['delta'] = errors[j] * sigmoidDerivative(neuron['output'])
+                neuron['delta'] = cost[j] * self.sigmoidDerivative(neuron['output'])
 
 
 
-def updateThetas(neuralNetwork, X, alpha):
-    for layerCounter in range(len(neuralNetwork)):
-        if layerCounter != 0:
-            X = [neuron['output'] for neuron in neuralNetwork[layerCounter - 1]]
-        for eachNeurons in neuralNetwork[layerCounter]:
-            for eachInput in range(len(X)):
-                eachNeurons['thetas'][eachInput] += alpha * eachNeurons['delta'] * X[eachInput]
-            eachNeurons['thetas'][-1] += alpha * eachNeurons['delta']
+    def updateThetas(self,neuralNetwork, X, alpha):
+        for layerCounter in range(len(neuralNetwork)):
+            if layerCounter != 0:
+                X = [neuron['output'] for neuron in neuralNetwork[layerCounter - 1]]
+            for eachNeurons in neuralNetwork[layerCounter]:
+                for eachInput in range(len(X)):
+                    eachNeurons['thetas'][eachInput] = eachNeurons['thetas'][eachInput]+alpha * eachNeurons['delta'] * X[eachInput]
+                eachNeurons['thetas'][-1] = eachNeurons['thetas'][-1] + alpha * eachNeurons['delta']
 
 
 
-def fit(neuralNetwork, Xtrain, YTrain, alpha, MAX_ITER, numberOfClassesToPredict):
+    def fit(self, Xtrain, YTrain, alpha, MAX_ITER, numberOfClassesToPredict):
 
-    for iterationCounter in range(MAX_ITER):
-        costDuringTraining = 0
-        for Xtrain_,YTrain_ in zip(Xtrain,YTrain):
-            outputs = feedForwardPropagation(neuralNetwork, Xtrain_)
-            oneHotEncodedOutput = oneHotEncoding(YTrain_,numberOfClassesToPredict)
-            costDuringTraining += sum([(oneHotEncodedOutput[encodedOutputCounter]-outputs[encodedOutputCounter])**2 for encodedOutputCounter in range(len(oneHotEncodedOutput))])
-            backward_propagate_error(neuralNetwork, oneHotEncodedOutput)
-            updateThetas(neuralNetwork, Xtrain_, alpha)
-
-
-def oneHotEncoding(YTrain_,numberOfClassesToPredict):
-    oneHotEncodedOutput = [0 for _ in range(numberOfClassesToPredict)]
-    oneHotEncodedOutput[YTrain_] = 1
-    return oneHotEncodedOutput
+        for iterationCounter in range(MAX_ITER):
+            #costDuringTraining = 0
+            for Xtrain_,YTrain_ in zip(Xtrain,YTrain):
+                #outputs = self.feedForwardPropagation(self.neuralNetwork, Xtrain_)
+                self.feedForwardPropagation(self.neuralNetwork, Xtrain_)
+                oneHotEncodedOutput = self.oneHotEncoding(YTrain_,numberOfClassesToPredict)
+                #costDuringTraining += sum([(oneHotEncodedOutput[encodedOutputCounter]-outputs[encodedOutputCounter])**2 for encodedOutputCounter in range(len(oneHotEncodedOutput))])
+                self.backPropagation(self.neuralNetwork, oneHotEncodedOutput)
+                self.updateThetas(self.neuralNetwork, Xtrain_, alpha)
 
 
 
@@ -108,26 +107,28 @@ def oneHotEncoding(YTrain_,numberOfClassesToPredict):
 
 
 
+    def oneHotEncoding(self,YTrain_,numberOfClassesToPredict):
+        oneHotEncodedOutput = [0 for _ in range(numberOfClassesToPredict)]
+        oneHotEncodedOutput[YTrain_] = 1
+        return oneHotEncodedOutput
+
+    def calculateZ(self,thetas, inputs):
+        z = thetas[-1]
+        for i in range(len(thetas)-1):
+            z += thetas[i] * inputs[i]
+        return z
+
+    def calculateSigmoid(self,z):
+        return 1.0 / (1.0 + exp(-z))
 
 
+    def sigmoidDerivative(self,output):
+        return output * (1.0 - output)
 
-def calculateZ(thetas, inputs):
-    z = thetas[-1]
-    for i in range(len(thetas)-1):
-        z += thetas[i] * inputs[i]
-    return z
-
-def calculateSigmoid(z):
-    return 1.0 / (1.0 + exp(-z))
-
-
-def sigmoidDerivative(output):
-    return output * (1.0 - output)
-
-def predict(neuralNetwork, X):
-    softmaxPredictions = feedForwardPropagation(neuralNetwork, X)
-    return softmaxPredictions.index(max(softmaxPredictions))
-seed(2)
+    def predict(self, X):
+        softmaxPredictions = self.feedForwardPropagation(self.neuralNetwork, X)
+        return softmaxPredictions.index(max(softmaxPredictions))
+    seed(2)
 
 
 
@@ -162,15 +163,15 @@ hiddenLayers = [5,7]
 
 
 
+clf = NeuralNetwork(numberofInputfeatures, hiddenLayers, numberofoutputFeatures)
 
-neuralNetwork = buildNetwork(numberofInputfeatures, hiddenLayers, numberofoutputFeatures)
-fit(neuralNetwork, XTrain, YTrain, alpha, maxIteration, numberofoutputFeatures)
+clf.fit( XTrain, YTrain, alpha, maxIteration, numberofoutputFeatures)
 
 
 
 predictionList = []
 for XTest_,YTest_ in zip(XTest,YTest):
-    prediction = predict(neuralNetwork, XTest_)
+    prediction = clf.predict(XTest_)
     predictionList.append(prediction)
 
 #print(YTest.values.tolist())
